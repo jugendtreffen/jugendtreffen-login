0bc85a30e1e0df501261db394f7867dd9b20a27b
David <dagrahxtgnq3@gmail.com>
2026-02-23 21:59:23 +0100

    refactor: schema

--- api/src/services/registerations/registeredParticipants.scenarios.ts
+++ api/src/services/registerations/registeredParticipants.scenarios.ts
@@ -1,4 +1,4 @@
-import type { Prisma, Participation } from '@prisma/client'
+import type { Participation, Prisma } from '@prisma/client'
 
 import type { ScenarioData } from '@redwoodjs/testing/api'
 
