19b5d4bec5a845e7212deceb071cf80d04941846
David Gruber <dagrahxtgnq3@gmail.com>
2026-04-14 21:59:40 +0200

    add: mailer service

---
+++ api/src/services/mailer/mailer.ts
@@ -0,0 +1,26 @@
+import {sendTemplateBrevoEmail} from "src/lib/brevoMailer";
+
+interface EmailParams {
+  to: string
+  subject: string
+  html?: string
+  text?: string
+  from?: string
+}
+
+export type WelcomeEmailInput = {
+  to: string
+  name: string
+}
+
+export async function sendRegistrationConfirmation({
+                                         to,
+                                         name,
+                                       }: WelcomeEmailInput) {
+  const templateId = "4"
+
+  return sendTemplateBrevoEmail({
+    to,
+    templateId,
+  })
+}
